462b39a438f7b166f41303fc52144db87598859d
Q_uan <quan_qixian@163.com>
2020-05-03 20:10:43 -0700

    3_3 Automatic type deduction with auto

---
+++ 3_3.auto.cpp
@@ -0,0 +1,35 @@
+/***************
+ * filename:
+ * date    :
+ * author  :
+ * function:
+ * ***************/
+#include <vector>
+#include <string>
+
+using namespace std;
+double f()
+{    
+	return 3.14;
+}
+
+int main(int argc,const char*argv[])
+{    
+	/**
+	 * With C++11,you can declare a variable or an object without specifying its specific tyoe by using atuo.
+	 * For example:
+	 **/
+	auto i = 42;//i has tyoe int 
+	auto d = f(); //d has type double 
+
+	/**
+	 * Using auto is especially useful where the type is a pretty long and/or
+	 * complicated expression.For example:
+	 */
+	vector<string> v;
+	//...
+	auto pos = v.begin();  //pos has tyoe vector<string>::iterator
+
+
+	return 0;
+}
