108c8f2bee729da4952d4d5b955219db0561ca8c
Q_uan <quan_qixian@163.com>
2020-10-28 23:07:55 +0800

    update 2_1.VariadicTemplates.cpp

--- 2_1.VariadicTemplates.cpp
+++ 2_1.VariadicTemplates.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <bitset>
+/**
+ * ...就是一个所谓的pack（包）
+ * 用于 template parameters，就是 template parameters pack（模板参数包）
+ * 用于 function paramerer types，就是 function parameter types pack（函数参数类型包）
+ * 用于 function parameters，就是function parameters pack（函数参数包）
+ */
 using namespace std;
 void print()
 {    
@@ -8,6 +14,9 @@ void print()
 template <typename T, typename... Types>
 void print(const T& firstArg, const Types&... args)
 {    
+	/**
+	 * Inside variadic templates, sizeof...(args) yields the number of arguments
+	 */
 	cout<<"    sizeof...(args)="<<sizeof...(args)<<endl;
 	cout<<firstArg<<endl;
 	print(args...);
