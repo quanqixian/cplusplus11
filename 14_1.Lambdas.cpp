41b20574f47341878776c8aef2be8fe33becbdde
Q_uan <quan_qixian@163.com>
2020-12-12 18:08:58 +0800

    update 14_1.Lambdas.cpp

--- 14_1.Lambdas.cpp
+++ 14_1.Lambdas.cpp
@@ -1,3 +1,13 @@
+/**
+ * C++11 introduced lambdas,allowing the definition of inline
+ * functionality,which can be used as a parameter or a local object, 
+ * Lambdas change the way the C++ standard library is used.
+ *
+ * A lambda is a definition of functionality that can be defined inside
+ * statements and expressions. Thus, you can use a lambda as an inline function.
+ * The minimal lambda function has no parameters and simply dose something:
+ */
+
 #include <iostream>
 /**
  *lambda基本用法实例
@@ -12,7 +22,7 @@ int main(int argc, const char *argv[])
 
 	[]{
 		std::cout<<"hello lambda"<<std::endl;
-	}();
+	}();/* 加()为调用 */
 
 	auto I = [] {
 		std::cout<<"hello lambda"<<std::endl;
